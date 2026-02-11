cac2c469b316163911afed7da95b730bc547bf3a
bhartiinsan <dukebhaartiiprofessional@gmail.com>
2026-02-11 17:34:46 +0530

    THis Is updated progress

---
+++ python/Day3/double quote.py
@@ -0,0 +1,22 @@
+print('''this is rishi's property we want to "sell"''')
+ 
+print('this is rishi\'s property we want to "sell"')
+#use to ignore appistrophy
+
+# print rushi
+
+print('"rushi"')
+
+#print tabspace rishi shyam ram
+
+print('rishi\tshyam\tram')
+
+#raw string
+print(r"Hello\nworld")   #it print as a literal not a new line
+print(r'He said "Hello"')
+print(rf"Sum={5+3}")
+
+print(f"Hi {20}")
+print("bharti\\t kk")  #when two backslashes are typed it count bydefault one
+print("bharti\n kk")   #when \n typed it create a new line character 
+
---
+++ python/Day3/ram shyam aam.py
@@ -0,0 +1,26 @@
+
+#
+print('"rushi" \n "shyam" \n "ram"')  # print as a new line character with quotes
+print("\n")
+print('rushi \n shyam \n ram') # print as a new line character with quotes
+print("\n")
+
+
+print('rushi \t shyam \t ram')
+print("\n")
+
+print (r'rishi \n shyam \n ram')   # it is a raw string with quotes which ignores escape processing and print literlly("as it is as written")
+print("\n")
+
+print('hello'+'  '+'world')
+
+print('hello'+'            world')
+
+print("\n")
+
+print("Python", end="   ❤️     ")
+print("Bharti")
+
+
+
+
