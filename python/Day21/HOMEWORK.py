95dcf0a17255e9a5ae549e49a6e64ac3ddffcf74
bhartiinsan <dukebhaartiiprofessional@gmail.com>
2026-03-18 23:42:52 +0530

    swapcases

---
+++ python/Day21/HOMEWORK.py
@@ -0,0 +1,19 @@
+# 1. Write a Python program to calculate the length of a string without using len(). 
+# s=input(" Enter the string")
+
+# count= 0
+
+# for char in s:
+#     count+=1
+
+# print(f"LENGTH OF A STRING is {count}")
+
+#----------------------------------------------------------------------------------------
+
+# 2. Write a Python program to change a given string to a new string where the first and last
+# chars have been exchanged. 
+
+s="I have a very Big dream"
+
+case=s.swapcase
+print(case)
