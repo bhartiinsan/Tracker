564abd5592ec429135046430c483eeb40282b0d6
bhartiinsan <dukebhaartiiprofessional@gmail.com>
2026-04-03 23:18:30 +0530

    TUPLE

---
+++ python/Day27/fun.py
@@ -0,0 +1,91 @@
+# IT CAN BE DUPLICATE 
+# IT MAINTAINS ORDER WHILE PRINTING
+
+t=(10,20,30,10)
+# print(t)
+
+#WE CAN PERFORM
+
+# INDEXING
+# print(t[0])
+
+# ITERABLE
+# for i in t:
+#     print(i)
+
+# MIN()
+# print(min(t))
+
+# MAX ()
+# print(max(t))
+
+# SUM ()
+# print(sum(t))
+
+# LEN()
+# print(len(t))  #4
+
+
+#SORTED()
+# print(sorted(t))
+# IT USES DOUBLE MEMORY WHILE SORTING AND CONVERT INTO LIST
+#[10, 10, 20, 30]
+
+
+# DIFFERENCE BETWEEN SORTED AND SORT
+
+# SORTED
+#  FUNCTION OF ITERABLE
+
+
+# SORT 
+# IT PERFORM ON CHANGEABLE OR MUTABLE THINGS
+# METHOD  OF LIST (APPEND, INSERT, REMOVE, CLEAR, POP)
+
+# IN 
+# print(50 in t)  #False
+
+# print(10 in t) # True
+
+#=============================================================================================================================
+
+#IF SOMEONE WANTS TO DO CHANGES IN TUPLE, THEN  CONVERT IT INTO LIST AND MAKE CHANGES INTO IT, WHILE PRINTING CONVERT INTO TUPLE
+
+# LIST
+
+# MAKKING PARALLEL MEMORY ALLOCATION CAUSE WE ARE DOING CONVERSION OF CAUSE WE ARE DOING CONVERSION OF TUPLE IN TO LIST
+
+# li=list(t)
+# print(li)
+
+# REPITITIONS
+# print(t*3)
+
+# CONCATENATION
+# print(t+t+t)
+
+#========================================================================================================================
+
+#IF WE DO HETEROGENOUS DATATYPES IN TUPLE 
+# THE OPERATIONS THAT NOT PERFORM IS 
+# MIN()
+#SUM()
+#SORTED()
+# MAX()
+
+#=========================================================================================================================
+
+#NESTED TUPLE 
+# N=((2,3,6),[8,9,8])
+# we can put list and tuple in tuple
+
+#=========================================================================================================================
+
+#BUT THERE IS NO COMPREHEND TUPLE IN EXIST
+# NO APPEND VALUES
+# NO REMOVE VALUES
+# NO EXTEND VALUES
+# NO INSERT VALUES
+# NO DEL VALUES
+
+
---
+++ python/Day27/tuple.py
@@ -0,0 +1,43 @@
+#TUPLE TOPOLOGY
+# IT IS IMMUTABLE {No change can be done}
+# IMMUTABLE VERSION OF LIST 
+
+
+
+#-----------------------------------------------
+
+# HERE ARE SOME USE CASE OF TUPLE
+#HISTORICAL DATA CAN BE MANAGED IN THIS
+#TRAVEL DATA ONE END TO ANOTHER 
+
+
+# -----------------------------------------------
+
+
+#()- SYMBOL
+# WHERE ELSE WE USED EARLIER 
+# print() - FUNCTION CALL
+#OPERATOR PRESEDENCE- HIGH PRIORITY TO THE BRACKET
+# M=(2+3)*4
+# print(M) =20
+
+#-----------------------------------------------------------
+
+t=(10)  # by default it takes this as presedence operator
+print(type(t),t) #int 10
+
+#===========================================================
+
+t=()
+print(type(t)) # tuple
+
+#===========================================================
+
+t=(10,) # WE NEED TO PUT THE COMMA IN IT TO MAKE TUPLE
+print(type(t)) # tuple
+
+
+
+
+
+
